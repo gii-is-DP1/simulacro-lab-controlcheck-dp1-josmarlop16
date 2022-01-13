a2b11db7cda22e970982647c107787679589ffa0
josmarlop16 <joseantonio@localhost>
2022-01-13 18:32:15 +0100

    Entrega

--- src/main/java/org/springframework/samples/petclinic/product/ProductService.java
+++ src/main/java/org/springframework/samples/petclinic/product/ProductService.java
@@ -2,21 +2,36 @@ package org.springframework.samples.petclinic.product;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class ProductService {
+
+    @Autowired ProductRepository productRepository;
+
     public List<Product> getAllProducts(){
-        return null;
+        List<Product> p = productRepository.findAll();
+        return p;
     }
 
     public List<Product> getProductsCheaperThan(double price) {
-        return null;
+        List<Product> p = productRepository.findbyPriceLessThan(price);
+        return p;
     }
 
     public ProductType getProductType(String typeName) {
-        return null;
+        ProductType p = productRepository.findProductType(typeName);
+        return p;
     }
 
     public Product save(Product p){
-        return null;       
+        return productRepository.save(p);      
+    }
+
+    public List<ProductType> findAllProductTypes() {
+        List<ProductType> p = productRepository.findAllProductTypes();
+        return p;
     }
 
     
